62e5fc38c730fa8d0173d5dc5c6bfda6bb5749fc
baishakicodes <43207472+baishakicodes@users.noreply.github.com>
2018-10-16 19:07:35 -0400

    Added in the already completed pentagon code and I added in new Rectangle code.

---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Rectangle extends Shape {
+    private int height;
+    private int width;
+    public Rectangle(int height, int width){
+        super("Rectangle");
+        this.height = height;
+        this.width = width;
+    }
+    @Override
+    public double getArea(){
+        return (height * width);
+    }
+    @Override
+    public double getPerimeter(){
+        return (2*height)+(2*width);
+    }
+}
--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -4,6 +4,9 @@ public class Runner {
 
     public static void main(String[] args) {
 	// write your code here
-        Shape shape1 
+    Shape shape1 = new Rectangle(2, 3);
+    System.out.println(shape1);
+    System.out.println("My area is "+ shape1.getArea());
+    System.out.println("My perimeter is "+shape1.getPerimeter());
     }
 }
