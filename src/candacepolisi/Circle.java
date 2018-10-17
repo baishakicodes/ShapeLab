5501c4e01faa4074141497bdb1f7c73ed8c5698b
BT_1N3_30 <cpolisi6526@bths.edu>
2018-10-17 08:18:51 -0400

    3

---
+++ src/candacepolisi/Circle.java
@@ -0,0 +1,16 @@
+package candacepolisi;
+import com.company.Shape;
+public class Circle {
+    public class Circle extends Shape {
+        private int radius;
+
+        public Circle(int radius){
+            super("Circle");
+            this.radius=radius;
+        }
+
+        public double getArea() {
+            double pi= Math.PI
+        }
+    }
+}
