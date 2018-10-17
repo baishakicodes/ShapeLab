5e51dafc11902506785dc8aec671cbe2d0a30abe
BT_1N3_31 <baishakicodes>
2018-10-17 08:43:02 -0400

    updated runner

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -8,13 +8,20 @@ public class Runner {
     public static void main(String[] args) {
 	// write your code here
     Shape shape1 = new ETriangle(2);
-    System.out.println(shape1);
-    System.out.println("My area is "+ shape1.getArea());
-    System.out.println("My perimeter is "+shape1.getPerimeter());
-
     Shape shape2 = new Circle(3);
-    System.out.println(shape2);
-    System.out.println("My area is " + shape2.getArea());
-    System.out.println("My perimeter is " + shape2.getPerimeter());
+    Shape shape3 = new Rectangle(3,2);
+
+    Shape shape4 = new RPentagon(5);
+    System.out.println(shape4);
+    System.out.println("My perimeter is: "+shape4.getPerimeter());
+    System.out.println("My area is: "+shape4.getArea());
+
+    Shape[] shapes = {shape1, shape2, shape3};
+    for (Shape cur : shapes){
+        System.out.println(cur);
+        System.out.println("My area is: "+cur.getArea());
+        System.out.println("My perimeter is: "+cur.getPerimeter());
+    }
+
     }
 }
